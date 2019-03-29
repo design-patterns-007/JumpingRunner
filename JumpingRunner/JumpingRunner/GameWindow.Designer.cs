dc880908a87f3b61478542473f0329484c81056c
Cristi <thiess.cristi@gmail.com>
2019-03-29 15:37:30 +0200

    Added the game window

---
+++ JumpingRunner/JumpingRunner/GameWindow.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace JumpingRunner
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBoxGame = new System.Windows.Forms.PictureBox();
+            this.LevelLabel = new System.Windows.Forms.Label();
+            this.ComboBoxLevel = new System.Windows.Forms.ComboBox();
+            this.LabelPlayerEquipment = new System.Windows.Forms.Label();
+            this.CheckBoxHat = new System.Windows.Forms.CheckBox();
+            this.CheckBoxSunGlasses = new System.Windows.Forms.CheckBox();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBoxGame
+            // 
+            this.PictureBoxGame.Location = new System.Drawing.Point(8, 90);
+            this.PictureBoxGame.Name = "PictureBoxGame";
+            this.PictureBoxGame.Size = new System.Drawing.Size(644, 281);
+            this.PictureBoxGame.TabIndex = 0;
+            this.PictureBoxGame.TabStop = false;
+            // 
+            // LevelLabel
+            // 
+            this.LevelLabel.AutoSize = true;
+            this.LevelLabel.Location = new System.Drawing.Point(26, 31);
+            this.LevelLabel.Name = "LevelLabel";
+            this.LevelLabel.Size = new System.Drawing.Size(62, 13);
+            this.LevelLabel.TabIndex = 1;
+            this.LevelLabel.Text = "Select level";
+            // 
+            // ComboBoxLevel
+            // 
+            this.ComboBoxLevel.FormattingEnabled = true;
+            this.ComboBoxLevel.Location = new System.Drawing.Point(108, 33);
+            this.ComboBoxLevel.Name = "ComboBoxLevel";
+            this.ComboBoxLevel.Size = new System.Drawing.Size(74, 21);
+            this.ComboBoxLevel.TabIndex = 2;
+            // 
+            // LabelPlayerEquipment
+            // 
+            this.LabelPlayerEquipment.AutoSize = true;
+            this.LabelPlayerEquipment.Location = new System.Drawing.Point(235, 35);
+            this.LabelPlayerEquipment.Name = "LabelPlayerEquipment";
+            this.LabelPlayerEquipment.Size = new System.Drawing.Size(88, 13);
+            this.LabelPlayerEquipment.TabIndex = 3;
+            this.LabelPlayerEquipment.Text = "Player equipment";
+            // 
+            // CheckBoxHat
+            // 
+            this.CheckBoxHat.AutoSize = true;
+            this.CheckBoxHat.Location = new System.Drawing.Point(329, 35);
+            this.CheckBoxHat.Name = "CheckBoxHat";
+            this.CheckBoxHat.Size = new System.Drawing.Size(43, 17);
+            this.CheckBoxHat.TabIndex = 4;
+            this.CheckBoxHat.Text = "Hat";
+            this.CheckBoxHat.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxSunGlasses
+            // 
+            this.CheckBoxSunGlasses.AutoSize = true;
+            this.CheckBoxSunGlasses.Location = new System.Drawing.Point(378, 34);
+            this.CheckBoxSunGlasses.Name = "CheckBoxSunGlasses";
+            this.CheckBoxSunGlasses.Size = new System.Drawing.Size(80, 17);
+            this.CheckBoxSunGlasses.TabIndex = 5;
+            this.CheckBoxSunGlasses.Text = "Sunglasses";
+            this.CheckBoxSunGlasses.UseVisualStyleBackColor = true;
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(532, 35);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 6;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 382);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.CheckBoxSunGlasses);
+            this.Controls.Add(this.CheckBoxHat);
+            this.Controls.Add(this.LabelPlayerEquipment);
+            this.Controls.Add(this.ComboBoxLevel);
+            this.Controls.Add(this.LevelLabel);
+            this.Controls.Add(this.PictureBoxGame);
+            this.Name = "GameWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxGame)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxGame;
+        private System.Windows.Forms.Label LevelLabel;
+        private System.Windows.Forms.ComboBox ComboBoxLevel;
+        private System.Windows.Forms.Label LabelPlayerEquipment;
+        private System.Windows.Forms.CheckBox CheckBoxHat;
+        private System.Windows.Forms.CheckBox CheckBoxSunGlasses;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
