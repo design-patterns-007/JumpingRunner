5ae1b288bbc527e65e23efc99c44ac161dccf1b2
stefy5551 <43267991+stefy5551@users.noreply.github.com>
2019-03-30 13:01:13 +0200

    Obstacle+Types

---
+++ JumpingRunner/JumpingRunner/Obstacle/Obstacle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace JumpingRunner.Obstacle
+{
+    class Obstacle
+    { 
+        public PointCollection PointCollection = new PointCollection();
+        public System.Drawing.Color Color { get; set; }
+        public int XVelocity { get; set; } = 10;
+        
+        public Obstacle(PointCollection points, System.Drawing.Color color)
+        {
+            PointCollection = points;
+            Color = color;
+        }
+
+        public void Update()
+        {
+            for(int i=0; i<PointCollection.Count(); i++)
+            {
+                PointCollection[i].Offset(-XVelocity, 0);
+            }
+        }
+
+        public void Paint()
+        {
+
+        }
+    }
+}
---
+++ JumpingRunner/JumpingRunner/Obstacle/Spike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace JumpingRunner.Obstacle
+{
+    class Spike : Obstacle
+    {
+        public Spike(PointCollection points, System.Drawing.Color color) : base(points, color)
+        {
+        }
+
+        public override EObstacleType Type()
+        {
+            return EObstacleType.Spike;
+        }
+    }
+}
